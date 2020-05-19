0626bf5af5758c7267db464da3859cd1e0a744a6
wangdong3 <wangdong3@beisen.com>
2020-05-19 11:34:58 +0800

    init project

---
+++ Code/Test.Chameleon.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Chameleon.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
