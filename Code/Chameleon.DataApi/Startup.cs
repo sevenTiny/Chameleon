c4f26a6ed8607de12a6fa97e073cf4b89c1187cd
wangdong3 <wangdong3@beisen.com>
2020-06-24 16:04:19 +0800

    remove uneccessary code

--- Code/Chameleon.DataApi/Startup.cs
+++ Code/Chameleon.DataApi/Startup.cs
@@ -1,21 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Encodings.Web;
-using System.Text.Unicode;
-using System.Threading.Tasks;
-using Chameleon.Application;
 using Chameleon.Bootstrapper;
-using Chameleon.Domain;
-using Chameleon.Infrastructure;
-using Chameleon.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 namespace Chameleon.DataApi
 {
