f94711d60c790da1a644f880950c6513e8d18943
wangdong3 <wangdong3@beisen.com>
2020-07-22 18:38:21 +0800

    导入功能

--- Code/Chameleon.Development/Controllers/CloudApplicationDeployController.cs
+++ Code/Chameleon.Development/Controllers/CloudApplicationDeployController.cs
@@ -2,6 +2,8 @@
 using Chameleon.Domain;
 using Chameleon.Entity;
 using Chameleon.Repository;
+using Chameleon.ValueObject;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SevenTiny.Bantina;
@@ -9,6 +11,7 @@ using SevenTiny.Bantina.Extensions.AspNetCore;
 using SevenTiny.Bantina.Validation;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Unicode;
@@ -62,9 +65,31 @@ namespace Chameleon.Development.Controllers
             return View(ResponseModel.Success());
         }
 
-        public IActionResult AllCloudApplicationImport()
+        public IActionResult AllCloudApplicationImport([FromForm]IFormCollection formData)
         {
-            return Ok();
+            IFormFileCollection files = formData.Files;
+
+            if (files == null || !files.Any())
+                return View("Import", ResponseModel.Error("未找到文件"));
+
+            var successList = new List<List<string>>(files.Count);
+
+            foreach (var item in files)
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    item.CopyTo(stream);
+                    var bytes = stream.ToArray();
+                    var content = Encoding.UTF8.GetString(bytes);
+                    var dto = JsonConvert.DeserializeObject<CloudApplicationDeployDto>(content);
+                    var result = _cloudApplicationDeployService.AllCloudApplicationImport(dto);
+
+                    if (result.IsSuccess)
+                        successList.Add(result.MoreMessage);
+                }
+            }
+
+            return Content(JsonConvert.SerializeObject(successList));
         }
     }
 }
