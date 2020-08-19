4895b8fd5b0ff9911bb9116bba420b405f5c605d
wangdong3 <wangdong3@beisen.com>
2020-08-19 16:35:59 +0800

    转移系统信息到account api

--- Code/Chameleon.DataApi/Controllers/UserAccountController.cs
+++ Code/Chameleon.DataApi/Controllers/UserAccountController.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using SevenTiny.Bantina;
 using SevenTiny.Bantina.Extensions.AspNetCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Chameleon.DataApi.Controllers
@@ -22,12 +24,20 @@ namespace Chameleon.DataApi.Controllers
         IUserAccountRepository _userAccountRepository;
         IUserAccountService _userAccountService;
         IUserAccountApp _userAccountApp;
-        public UserAccountController(IUserAccountApp userAccountApp, IUserAccountService userAccountService, IUserAccountRepository userAccountRepository, IOrganizationService organizationService)
+        IProfileRepository _profileRepository;
+        IMenuRepository _menuRepository;
+        IFunctionService _functionService;
+        IMenuService _menuService;
+        public UserAccountController(IMenuService menuService, IFunctionService functionService, IUserAccountRepository userAccountRepository, IMenuRepository menuRepository, IProfileRepository profileRepository, IUserAccountService userAccountService, IUserAccountApp userAccountApp, IOrganizationService organizationService)
         {
             _userAccountApp = userAccountApp;
-            _userAccountService = userAccountService;
-            _userAccountRepository = userAccountRepository;
             _organizationService = organizationService;
+            _menuService = menuService;
+            _functionService = functionService;
+            _userAccountRepository = userAccountRepository;
+            _menuRepository = menuRepository;
+            _profileRepository = profileRepository;
+            _userAccountService = userAccountService;
         }
 
         private string GetTokenAndSaveCookie(UserAccount userAccount)
@@ -39,6 +49,75 @@ namespace Chameleon.DataApi.Controllers
 
             return token;
         }
+
+        [Route("ChameleonSystemInfo")]
+        public IActionResult ChameleonSystemInfo()
+        {
+            return SafeExecute(() =>
+            {
+                var userAccount = _userAccountRepository.GetUserAccountByUserId(CurrentUserId);
+
+                if (userAccount == null)
+                    return Result.Error("用户不存在").ToJsonResult();
+
+                var profile = _profileRepository.GetById(userAccount.Profile);
+
+                if (profile == null)
+                    return Result.Error("身份不正确").ToJsonResult();
+
+                //有权限的功能
+                var permissionFunctions = new HashSet<string>(_functionService.GetFunctionArrayFromString(profile.PermissionFunction));
+
+                var allFunctions = _functionService.GetListUnDeleted();
+
+                //有权限的菜单
+                var permissionMenus = new HashSet<string>(_menuService.GetMenuArrayFromString(profile.PermissionMenu));
+
+                var chameleonSystemInfo = new ChameleonSystemInfoDto
+                {
+                    UserId = userAccount.UserId,
+                    UserEmail = userAccount.Email,
+                    UserProfileId = userAccount.Profile,
+                    UserRoleId = userAccount.Role,
+                    AvatarPicId = userAccount.AvatarPicId,
+                    ViewFunction = allFunctions.Where(t => permissionFunctions.Contains(t.Id.ToString())).Select(t => t.Code).ToList(),
+                    ViewMenu = GetTree(permissionMenus)
+                };
+
+                return ResponseModel.Success(data: chameleonSystemInfo).ToJsonResult();
+            });
+        }
+
+        private List<MenuView> GetTree(HashSet<string> permissionSet)
+        {
+            //获取所有子节点
+            var nodes = _menuRepository.GetListUnDeleted();
+
+            //获取顶级节点
+            Menu condition = nodes?.FirstOrDefault(t => t.ParentId == Guid.Empty);
+
+            if (condition == null)
+                return new List<MenuView>(0);
+
+            return GetTree(nodes, condition.Id);
+
+            //Tree Search
+            List<MenuView> GetTree(List<Menu> source, Guid parentId)
+            {
+                var childs = source.Where(t => t.ParentId == parentId && permissionSet.Contains(t.Id.ToString()))
+                    .OrderBy(t => t.SortNumber)
+                    .Select(t => new MenuView { Id = t.Id, Name = t.Name, Route = t.Route })
+                    .ToList();
+
+                if (childs == null)
+                    return new List<MenuView>(0);
+
+                childs.ForEach(t => t.Children = GetTree(source, t.Id));
+
+                return childs;
+            }
+        }
+
 
         /// <summary>
         /// 第三方登陆，返回授权token
